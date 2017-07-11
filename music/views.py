fe188538ab1e656e0b381897f4d056f340786353
yogimishra96 <yogi@yogi~>
2017-07-11 22:41:06 +0530

    Connecting to the database

--- music/views.py
+++ music/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Album 
 
-# Create your views here.
+def index(request):
+    all_albums = Album.objects.all()
+    html = ''   
+    for album in all_albums: 
+        url   = '/music/' + str(album.id) + '/'
+        html += '<a href = "' + url + '">' + album.album_title + '</a><br>'  
+    return HttpResponse(html) 
+
+def detail(request, album_id):
+    return HttpResponse("<h2>Album id: " + str(album_id) + "</h2>")
